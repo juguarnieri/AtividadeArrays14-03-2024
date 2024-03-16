d07db05bc5fcdd6188b82efa3e63705092522b77
JúliaGuarnieri <juliaguarnieri04@gmail.com>
2024-03-16 13:33:35 -0300

    exemplo 2

--- join4.js
+++ join4.js
@@ -6,6 +6,19 @@ let palavras = ['Viver', 'é', 'desenhar', 'sem', 'borracha'];
 console.log("A frase do dia é:")// mensagem inicial.
 
 //O método join() é usado para juntar todos os elementos do array palavras em uma única string. 
-//O argumento " " passado para o método join() é o separador que será utilizado entre os elementos. Utilizei o espaço em branco, cada elemento é separado por ele. 
+//O argumento " "  é o separador que será utilizado entre os elementos. Utilizei o espaço em branco, cada elemento é separado por ele. 
 console.log(palavras.join(" "));
 
+
+//Exemplo 2:
+
+//Um array chamado names é declarado e inicializado com cinco strings.
+let names = ['Renata', 'Madalena', 'Carlos', 'Gustavo', 'Livia'];
+
+console.log("Lista de Alunos Aprovados do ano:");//mensagem inicial para mostrar os alunos aprovados.
+
+//O método join() é usado para juntar todos os elementos do array names em uma única string. 
+//O argumento "; " é o separador que será utilizado entre os elementos. Neste caso, estou usando ", ". 
+console.log(names.join(", ")); 
+
+console.log("Parabéns a todos os envolvidos!!!");//mensagem final dando os parabéns aos alunos que passaram.
